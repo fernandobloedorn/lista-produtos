c4d2f95be750f823dfc88a6819e2fe26f9671a9e
fernandobloedorn <fernandobloedorn@gmail.com>
2020-03-31 15:05:00 -0300

    Updates

---
+++ precache-manifest.d6d2dd75f465ce6dd7bc255af73ae657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120825172568d49b570bf6ad4e96a50c",
+    "url": "/fernandobloedorn/lista-produtos/index.html"
+  },
+  {
+    "revision": "43e83586c7b99c1d1bcc",
+    "url": "/fernandobloedorn/lista-produtos/static/css/main.1c25d7b7.chunk.css"
+  },
+  {
+    "revision": "91b76eccb112ff4087d9",
+    "url": "/fernandobloedorn/lista-produtos/static/js/2.d428bf0c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fernandobloedorn/lista-produtos/static/js/2.d428bf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e83586c7b99c1d1bcc",
+    "url": "/fernandobloedorn/lista-produtos/static/js/main.a3ffb203.chunk.js"
+  },
+  {
+    "revision": "6dc8c662eb1bcde4c12e",
+    "url": "/fernandobloedorn/lista-produtos/static/js/runtime-main.f159498a.js"
+  }
+]);
